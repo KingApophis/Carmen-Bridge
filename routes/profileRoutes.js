26419ad8358c9cbbd7a858354a84b02545457a92
Rohit-Mahesh <rohitmay62000@gmail.com>
2021-05-25 19:08:11 +0530

    security updates

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,75 +1,82 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const User = require('../schemas/UserSchema');
+const User = require("../schemas/UserSchema");
+const sanitize = require("mongo-sanitize");
 
 router.get("/", (req, res, next) => {
+  var payload = {
+    pageTitle: req.session.user.username,
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    profileUser: req.session.user,
+  };
 
-    var payload = {
-        pageTitle: req.session.user.username,
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-        profileUser: req.session.user
-    }
-    
-    res.status(200).render("profilePage", payload);
-})
+  res.status(200).render("profilePage", payload);
+});
 
 router.get("/:username", async (req, res, next) => {
+  var payload = await getPayload(
+    sanitize(req.params.username),
+    req.session.user
+  );
 
-    var payload = await getPayload(req.params.username, req.session.user);
-    
-    res.status(200).render("profilePage", payload);
-})
+  res.status(200).render("profilePage", payload);
+});
 
 router.get("/:username/replies", async (req, res, next) => {
+  var payload = await getPayload(
+    sanitize(req.params.username),
+    req.session.user
+  );
+  payload.selectedTab = "replies";
 
-    var payload = await getPayload(req.params.username, req.session.user);
-    payload.selectedTab = "replies";
-    
-    res.status(200).render("profilePage", payload);
-})
+  res.status(200).render("profilePage", payload);
+});
 
 router.get("/:username/following", async (req, res, next) => {
+  var payload = await getPayload(
+    sanitize(req.params.username),
+    req.session.user
+  );
+  payload.selectedTab = "following";
 
-    var payload = await getPayload(req.params.username, req.session.user);
-    payload.selectedTab = "following";
-    
-    res.status(200).render("followersAndFollowing", payload);
-})
+  res.status(200).render("followersAndFollowing", payload);
+});
 
 router.get("/:username/followers", async (req, res, next) => {
+  var payload = await getPayload(
+    sanitize(req.params.username),
+    req.session.user
+  );
+  payload.selectedTab = "followers";
 
-    var payload = await getPayload(req.params.username, req.session.user);
-    payload.selectedTab = "followers";
-    
-    res.status(200).render("followersAndFollowing", payload);
-})
+  res.status(200).render("followersAndFollowing", payload);
+});
 
 async function getPayload(username, userLoggedIn) {
-    var user = await User.findOne({ username: username })
+  var user = await User.findOne({ username: username });
 
-    if(user == null) {
+  if (user == null) {
+    user = await User.findById(username);
 
-        user = await User.findById(username);
-
-        if (user == null) {
-            return {
-                pageTitle: "User not found",
-                userLoggedIn: userLoggedIn,
-                userLoggedInJs: JSON.stringify(userLoggedIn)
-            }
-        }
-    }
-
-    return {
-        pageTitle: user.username,
+    if (user == null) {
+      return {
+        pageTitle: "User not found",
         userLoggedIn: userLoggedIn,
         userLoggedInJs: JSON.stringify(userLoggedIn),
-        profileUser: user
+      };
     }
+  }
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn: userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  };
 }
 
 module.exports = router;
